d5d67957618afd442141dfdb2ca91da146aa42b0
andersp89 <anders@ptrading.dk>
2018-03-24 12:57:45 +0100

    PEP8 Style formatting

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,44 +1,48 @@
+#!/usr/bin/python
+
 import media
 import fresh_tomatoes
 
-# File, to store 
-
+# Constructors of class Movie with information on favorite movies
 toy_story = media.Movie("Toy Story",
                         "A story of a boy and his toys",
-                        "http://www.heyuguys.com/images/2010/08/Toy-Story-3-Poster2.jpg",
+                        "http://www.heyuguys.com/images/2010/08/Toy-Story-3" +
+                        "-Poster2.jpg",
                         "https://www.youtube.com/watch?v=KYz2wyBy3kc")
 
 avatar = media.Movie("Avatar",
                      "A story about weirdos",
-                     "http://www.heyuguys.com/images/2010/08/Toy-Story-3-Poster2.jpg",
+                     "http://www.heyuguys.com/images/2010/08/Toy-Story-3-" +
+                     "Poster2.jpg",
                      "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
 
 
 school_of_rock = media.Movie("School of Rock",
                              "A story about weirdos",
-                             "http://www.heyuguys.com/images/2010/08/Toy-Story-3-Poster2.jpg",
-                             "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
+                             "http://www.heyuguys.com/images/2010/08/Toy-" +
+                             "Story-3-Poster2.jpg",
+                             "https://www.youtube.com/watch?v=XCwy6lW5Ixc")
 
-ratatuille = media.Movie("School of Rock",
-                             "A story about weirdos",
-                             "http://www.heyuguys.com/images/2010/08/Toy-Story-3-Poster2.jpg",
-                             "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
+ratatuille = media.Movie("Ratatuille",
+                         "A story about weirdos",
+                         "http://www.heyuguys.com/images/2010/08/Toy-Story" +
+                         "-3-Poster2.jpg",
+                         "https://www.youtube.com/watch?v=c3sBBRxDAqk")
 
-midnight_in_paris = media.Movie("School of Rock",
-                             "A story about weirdos",
-                             "http://www.heyuguys.com/images/2010/08/Toy-Story-3-Poster2.jpg",
-                             "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
+midnight_in_paris = media.Movie("Midnight in Paris",
+                                "A story about weirdos",
+                                "http://www.heyuguys.com/images/2010/08/Toy" +
+                                "-Story-3-Poster2.jpg",
+                                "https://www.youtube.com/watch?v=FAfR8omt-CY")
 
-hunger_games = media.Movie("School of Rock",
-                             "A story about weirdos",
-                             "http://www.heyuguys.com/images/2010/08/Toy-Story-3-Poster2.jpg",
-                             "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
+hunger_games = media.Movie("Hunger games",
+                           "A story about weirdos",
+                           "http://www.heyuguys.com/images/2010/08/Toy-" +
+                           "Story-3-Poster2.jpg",
+                           "https://www.youtube.com/watch?v=4S9a5V9ODuY")
 
-movies = [toy_story,avatar,school_of_rock, ratatuille, midnight_in_paris, hunger_games]
+all_movies = [toy_story, avatar, school_of_rock, ratatuille,
+              midnight_in_paris, hunger_games]
 
-fresh_tomatoes.open_movies_page(movies)
-#print(media.Movie.VALID_RATINGS)
-print(media.Movie.__doc__)
-print(media.Movie.__name__)
-print(media.Movie.__module__)
-print(media.Movie.__bases__)
+# Run function, to create fresh_tomatoes.html
+fresh_tomatoes.open_movies_page(all_movies)
